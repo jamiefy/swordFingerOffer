ef582a51a55bc16e960c0007be4f0750e147df19
翟安然git config --global user.name 翟安然 <1369296693@qq.com>
2019-11-14 13:31:56 +0800

    '第4题构造二叉树迭代器循环计数引用传递'

--- reconstructBinaryTree.cpp
+++ reconstructBinaryTree.cpp
@@ -13,41 +13,53 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-
+//递归 泛型算法 迭代器
 TreeNode* reConstructBinaryTree(std::vector<int> pre,std::vector<int> vin) {
     if(pre.size()==0 || vin.size()==0)
         return nullptr;
+    //构造节点指针，且分配空间
     TreeNode* node=new TreeNode(pre[0]);
     auto it=find(vin.begin(),vin.end(),pre[0]);
-    int leftTreeLength=it-vin.begin();
-    std::vector<int> leftTreePre(pre.begin()+1,pre.begin()+leftTreeLength+1);
+    int middle=it-vin.begin();
+    std::vector<int> leftTreePre(pre.begin()+1,pre.begin()+middle+1);
     std::vector<int> leftTreeVin(vin.begin(),it);
     node->left=reConstructBinaryTree(leftTreePre,leftTreeVin);
-    int rightTreeLength=vin.end()-it-1;
-    std::vector<int> rightTreePre(pre.begin()+rightTreeLength,pre.end());
+    std::vector<int> rightTreePre(pre.begin()+middle+1,pre.end());
     std::vector<int> rightTreeVin(it+1,vin.end());
     node->right=reConstructBinaryTree(rightTreePre,rightTreeVin);
     return node;
 }
 
-//智能指针更安全
-TreeNode* reConstructBinaryTreeShared(std::vector<int> pre,std::vector<int> vin) {
-    if(pre.size()==0 || vin.size()==0)
+//递归 引用传递 值传递
+TreeNode* reConstruct(std::vector<int> &pre,std::vector<int> &vin,int leftPreStart,int leftPreEnd,int rightVinStart,int rightVinEnd){
+    if(leftPreStart>leftPreEnd || rightVinStart>rightVinEnd){
         return nullptr;
-    std::shared_ptr<TreeNode> node=std::make_shared<TreeNode>(pre[0]);
-    auto it=find(vin.begin(),vin.end(),pre[0]);
-    int leftTreeLength=it-vin.begin();
-    std::vector<int> leftTreePre(pre.begin()+1,pre.begin()+leftTreeLength+1);
-    std::vector<int> leftTreeVin(vin.begin(),it);
-    node->left=reConstructBinaryTree(leftTreePre,leftTreeVin);
-    int rightTreeLength=vin.end()-it-1;
-    std::vector<int> rightTreePre(pre.begin()+rightTreeLength,pre.end());
-    std::vector<int> rightTreeVin(it+1,vin.end());
-    node->right=reConstructBinaryTree(rightTreePre,rightTreeVin);
-    return node.get();
+    }
+
+    //std::shared_ptr<TreeNode> node=std::shared_ptr<TreeNode>(new TreeNode(pre[leftPreStart]));会出现指针异常：不能采用智能指针，智能指针会自动析构，无法找到原来的地址内容
+    TreeNode* node=new TreeNode(pre[leftPreStart]);
+    int middle;
+    for(int i=0;i<vin.size();i++){
+        if(vin[i]==pre[leftPreStart]){
+            middle=i;
+            break;
+        }
+    }
+//    auto it=find(vin.begin(),vin.end(),pre[leftPreStart]);
+//    int middle=it-vin.begin()-rightVinStart;
+    node->left=reConstruct(pre,vin,leftPreStart+1,leftPreStart+middle,rightVinStart,rightVinStart+middle-1);
+    node->right=reConstruct(pre,vin,leftPreStart+middle+1,leftPreEnd,rightVinStart+middle+1,rightVinEnd);
+    return node;
 }
 
+TreeNode* reConstructBinaryTreeReference(std::vector<int> pre,std::vector<int> vin) {
+    if(pre.size()==0 || vin.size()==0)
+        return nullptr;
+    return reConstruct(pre,vin,0,pre.size()-1,0,vin.size()-1);
+}
+
+
 int main(){
-    TreeNode* head=reConstructBinaryTreeShared(std::vector<int>{1,2,4,7,3,5,6,8},std::vector<int>{4,7,2,1,5,3,8,6});
+    TreeNode* head=reConstructBinaryTree(std::vector<int>{1,2,4,7,3,5,6,8},std::vector<int>{4,7,2,1,5,3,8,6});
     std::cout<<head->val;
 }
